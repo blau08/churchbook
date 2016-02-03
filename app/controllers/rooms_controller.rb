8dc8d9bb0c7a5a8f4c14f8a847fee0e6cd0a79df
Brian Lau <blau08@gmail.com>
2016-02-02 22:14:39 -0800

    add reservation form and view

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,34 @@
 class RoomsController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
+    @students = Student.all
+  end
+
+  def new
+    @room = Room.new
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to rooms_path
+    else
+      render :new
+    end
+  end
+
+private
+
+  def sort_column
+    Student.column_names.include?(params[:sort]) ? params[:sort] : "lastname"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  def student_params
+    params.require(:room).permit(:clock, :name, :description, :room)
   end
 end
