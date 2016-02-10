ea8702d650532d85a0b94726d05021a457985371
Brian Lau <blau08@gmail.com>
2016-02-09 17:57:14 -0800

    add rescue for admin deny

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     session[:previous_url] || root_path
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+  redirect_to main_app.root_path, :alert => exception.message
+end
 end
